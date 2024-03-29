be3df3f226595958e638ca531202de99325f460e
James7z <james7.ca@gmail.com>
2024-03-28 21:03:10 -0400

    update test cases

--- PawMates/PawMates.Integration.Tests/ParentAPITests.cs
+++ PawMates/PawMates.Integration.Tests/ParentAPITests.cs
@@ -264,7 +264,10 @@ namespace PawMates.Integration.Tests
             int id = 999;
             var expected = HttpStatusCode.NotFound;
             //Act
-
+            var content = new StringContent(JsonConvert.SerializeObject(expected), Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync($"/api/parents/{id}", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<PetParentDTO>(responseContent);
             //Assert
 
         }
@@ -276,7 +279,10 @@ namespace PawMates.Integration.Tests
             int id = 999;
             var expected = HttpStatusCode.NotFound;
             //Act
-
+            var content = new StringContent(JsonConvert.SerializeObject(expected), Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync($"/api/parents/{id}", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<PetParentDTO>(responseContent);
             //Assert
 
         }
